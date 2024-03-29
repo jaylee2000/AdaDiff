0747dc87ea04449aea8a8dd7711f7552e65e4e10
Jeongjae Lee <jaysquirrel2000@gmail.com>
2024-03-29 16:13:45 +0900

    refactor: organize inessential functions into separate script under utils folder

--- train_adadiff_singlecoil.py
+++ train_adadiff_singlecoil.py
@@ -1,88 +1,23 @@
 import os
-import shutil
 import argparse
 import torchvision
 import torch
 import numpy as np
 
-from datasets_prep.brain_datasets import CreateTrainDataset
 from utils.args_op import add_singlecoil_train_args
-from utils.models.discriminator import Discriminator_large
-from utils.models.ncsnpp_generator_adagn import NCSNpp
-from utils.EMA import EMA
+from utils.setup_distributed_op import setup_distributed_environment, \
+                                       cleanup_distributed_environment
+from utils.setup_train_op import set_seed, prepare_dataset, initialize_models, \
+                                 broadcast_params, load_checkpoint, \
+                                 setup_optim_schedulers, setup_experiment_directory
 
 PARENT_BASE_DIR = "../diffusion_test"
 MASTER_PORT = '6025'
 
-def set_seed(seed_no):
-    torch.manual_seed(seed_no)
-    torch.cuda.manual_seed(seed_no)
-    torch.cuda.manual_seed_all(seed_no)
-    return
-
-def prepare_dataset(rank, args):
-    """ Create dataset. Return train sampler, data loader, and args. """
-    dataset = CreateTrainDataset()
-    train_sampler = torch.utils.data.distributed.DistributedSampler(
-                        dataset, num_replicas=args.world_size, rank=rank)
-    data_loader = torch.utils.data.DataLoader(
-                        dataset, batch_size=args.batch_size, sampler=train_sampler,
-                        shuffle=False, num_workers=4, pin_memory=True, drop_last=True)
-    return train_sampler, data_loader, args
-
-def initialize_models(args, device):
-    """Initialize the Generator and Discriminator models."""
-    netG = NCSNpp(args).to(device)
-    netD = Discriminator_large(nc=2*args.num_channels, ngf=args.ngf,
-                               t_emb_dim=args.t_emb_dim, act=torch.nn.LeakyReLU(0.2)).to(device)
-    netG = torch.nn.parallel.DistributedDataParallel(netG, device_ids=[device])
-    netD = torch.nn.parallel.DistributedDataParallel(netD, device_ids=[device])
-    return netG, netD
-
-def setup_optim_schedulers(netG, netD, args):
-    """Setup the optimizers and schedulers for both models."""
-    optimizerG = torch.optim.Adam(netG.parameters(), lr=args.lr_g, betas=(args.beta1, args.beta2))
-    optimizerD = torch.optim.Adam(netD.parameters(), lr=args.lr_d, betas=(args.beta1, args.beta2))
-    if args.use_ema:
-        optimizerG = EMA(optimizerG, ema_decay=args.ema_decay)
-
-    schedulerG = torch.optim.lr_scheduler.CosineAnnealingLR(optimizerG, args.num_epoch,
-                                                            eta_min=1e-5)
-    schedulerD = torch.optim.lr_scheduler.CosineAnnealingLR(optimizerD, args.num_epoch,
-                                                            eta_min=1e-5)
-    return optimizerG, optimizerD, schedulerG, schedulerD
-
-def setup_experiment_directory(exp_path):
-    if not os.path.exists(exp_path):
-        os.makedirs(exp_path)
-        copy_source(__file__, exp_path)
-        shutil.copytree('utils/models', os.path.join(exp_path, 'utils/models'))
-
 def initialize_training_components(args, device):
-    """ Initialize the training components. """
     coeff = Diffusion_Coefficients(args, device)
     pos_coeff = Posterior_Coefficients(args, device)
-    T = get_time_schedule(args, device)
-    return coeff, pos_coeff, T
-
-def load_checkpoint(exp_path, device, netG, optimizerG, schedulerG, netD, optimizerD, schedulerD):
-    checkpoint_file = os.path.join(exp_path, 'content.pth')
-    checkpoint = torch.load(checkpoint_file, map_location=device)
-    netG.load_state_dict(checkpoint['netG_dict'])
-    optimizerG.load_state_dict(checkpoint['optimizerG'])
-    schedulerG.load_state_dict(checkpoint['schedulerG'])
-    netD.load_state_dict(checkpoint['netD_dict'])
-    optimizerD.load_state_dict(checkpoint['optimizerD'])
-    schedulerD.load_state_dict(checkpoint['schedulerD'])
-    print(f"* loaded checkpoint (epoch {checkpoint['epoch']})")
-    return checkpoint['epoch'], checkpoint['global_step']
-
-def copy_source(file, output_dir):
-    shutil.copyfile(file, os.path.join(output_dir, os.path.basename(file)))
-            
-def broadcast_params(params):
-    for param in params:
-        torch.distributed.broadcast(param.data, src=0)
+    return coeff, pos_coeff
 
 #%% Diffusion coefficients 
 def var_func_vp(t, beta_min, beta_max):
@@ -98,14 +33,6 @@ def extract(input, t, shape):
     reshape = [shape[0]] + [1] * (len(shape) - 1)
     out = out.reshape(*reshape)
     return out
-
-def get_time_schedule(args, device):
-    n_timestep = args.num_timesteps
-    eps_small = 1e-3
-    t = np.arange(0, n_timestep + 1, dtype=np.float64)
-    t = t / n_timestep
-    t = torch.from_numpy(t) * (1. - eps_small)  + eps_small
-    return t.to(device)
 
 def get_sigma_schedule(args, device):
     n_timestep = args.num_timesteps
@@ -199,13 +126,13 @@ def sample_posterior(coefficients, x_0,x_t, t):
     sample_x_pos = p_sample(x_0, x_t, t)
     return sample_x_pos
 
-def sample_from_model(coefficients, generator, n_time, x_init, T, opt):
+def sample_from_model(coefficients, generator, n_time, x_init, options):
     x = x_init
     with torch.no_grad():
         for i in reversed(range(n_time)):
             t = torch.full((x.size(0),), i, dtype=torch.int64).to(x.device)
             t_time = t
-            latent_z = torch.randn(x.size(0), opt.nz, device=x.device)#.to(x.device)
+            latent_z = torch.randn(x.size(0), options.nz, device=x.device)#.to(x.device)
             x_0 = generator(x, t_time, latent_z)
             x_new = sample_posterior(coefficients, x_0, x, t)
             x = x_new.detach()
@@ -230,7 +157,7 @@ def train(rank, gpu, args):
     if rank == 0:
         setup_experiment_directory(exp_path)
 
-    coeff, pos_coeff, T = initialize_training_components(args, device)
+    coeff, pos_coeff = initialize_training_components(args, device)
 
     if args.resume:
         init_epoch, global_step = load_checkpoint(exp_path, device, netG, optimizerG,
@@ -241,21 +168,24 @@ def train(rank, gpu, args):
     # Main Training Loop
     for epoch in range(init_epoch, args.num_epoch+1):
         train_sampler.set_epoch(epoch)
-        for iteration, (x, y) in enumerate(data_loader):
+        for iteration, (x, y) in enumerate(data_loader): # label y is unused
+            # initialize D
             for p in netD.parameters():  
                 p.requires_grad = True
             netD.zero_grad()
-            #sample from p(x_0)
             real_data = x.to(device, non_blocking=True)
-            x_t_1 = torch.randn_like(real_data)
-            #sample t
+
+            # sample t
             t = torch.randint(0, args.num_timesteps, (real_data.size(0),), device=device)
             x_t, x_tp1 = q_sample_pairs(coeff, real_data, t)
             x_t.requires_grad = True
-            # train with real
+
+            # train Discriminator with real: forward pass
             D_real = netD(x_t, t, x_tp1.detach()).view(-1)
             errD_real = torch.nn.functional.softplus(-D_real)
             errD_real = errD_real.mean()
+
+            # train Discriminator with real: backward pass & gradient penalty
             errD_real.backward(retain_graph=True)
             if args.lazy_reg is None:
                 grad_real = torch.autograd.grad(
@@ -278,21 +208,25 @@ def train(rank, gpu, args):
                     grad_penalty = args.r1_gamma / 2 * grad_penalty
                     grad_penalty.backward()
 
-            # train with fake
+            # train Discriminator with fake: forward pass
             latent_z = torch.randn(args.batch_size, args.nz, device=device)
             x_0_predict = netG(x_tp1.detach(), t, latent_z)
             x_pos_sample = sample_posterior(pos_coeff, x_0_predict, x_tp1, t)
-            output = netD(x_pos_sample, t, x_tp1.detach()).view(-1)
-            errD_fake = torch.nn.functional.softplus(output)
+            D_fake = netD(x_pos_sample, t, x_tp1.detach()).view(-1)
+            errD_fake = torch.nn.functional.softplus(D_fake)
             errD_fake = errD_fake.mean()
+
+            # train Discriminator with fake: backward pass & gradient update
             errD_fake.backward()
             errD = errD_real + errD_fake
-            # Update D
-            optimizerD.step()
-            #update G
+            optimizerD.step() # update D gradient
+
+            # initialize G
             for p in netD.parameters():
                 p.requires_grad = False
             netG.zero_grad()
+
+            # train Generator: forward pass
             t = torch.randint(0, args.num_timesteps, (real_data.size(0),), device=device)
             x_t, x_tp1 = q_sample_pairs(coeff, real_data, t)
             latent_z = torch.randn(args.batch_size, args.nz, device=device)
@@ -301,25 +235,30 @@ def train(rank, gpu, args):
             output = netD(x_pos_sample, t, x_tp1.detach()).view(-1)
             errG = torch.nn.functional.softplus(-output)
             errG = errG.mean()
+
+            # train Generator: backward pass & gradient update
             errG.backward()
             optimizerG.step()
             global_step += 1
             if iteration % 100 == 0:
                 if rank == 0:
-                    print('epoch {} iteration{}, G Loss: {}, D Loss: {}'.format(epoch,iteration, errG.item(), errD.item()))
-        
+                    print(f'epoch {epoch} iteration{iteration},\n' \
+                          f'G Loss: {errG.item()}, D Loss: {errD.item()}')
+
         if not args.no_lr_decay:
             schedulerG.step()
             schedulerD.step()
 
         if rank == 0:
+            # save sample images
             if epoch % 10 == 0:
                 torchvision.utils.save_image(x_pos_sample, os.path.join(exp_path, 'xpos_epoch_{}.png'.format(epoch)), normalize=True)
-            
+
             x_t_1 = torch.randn_like(real_data)
-            fake_sample = sample_from_model(pos_coeff, netG, args.num_timesteps, x_t_1, T, args)
+            fake_sample = sample_from_model(pos_coeff, netG, args.num_timesteps, x_t_1, args)
             torchvision.utils.save_image(fake_sample, os.path.join(exp_path, 'sample_discrete_epoch_{}.png'.format(epoch)), normalize=True)
-            
+
+            # save model
             if args.save_content:
                 if epoch % args.save_content_every == 0:
                     print('Saving content.')
@@ -327,9 +266,10 @@ def train(rank, gpu, args):
                                'netG_dict': netG.state_dict(), 'optimizerG': optimizerG.state_dict(),
                                'schedulerG': schedulerG.state_dict(), 'netD_dict': netD.state_dict(),
                                'optimizerD': optimizerD.state_dict(), 'schedulerD': schedulerD.state_dict()}
-                    
+
                     torch.save(content, os.path.join(exp_path, 'content.pth'))
-                
+
+            # save checkpoint
             if epoch % args.save_ckpt_every == 0:
                 if args.use_ema:
                     optimizerG.swap_parameters_with_ema(store_params_in_ema=True)
@@ -337,18 +277,6 @@ def train(rank, gpu, args):
                 torch.save(netG.state_dict(), os.path.join(exp_path, 'netG_{}.pth'.format(epoch)))
                 if args.use_ema:
                     optimizerG.swap_parameters_with_ema(store_params_in_ema=True)
-
-def setup_distributed_environment(rank, size, master_addr, master_port):
-    """ Initialize the distributed environment. """
-    os.environ['MASTER_ADDR'] = master_addr
-    os.environ['MASTER_PORT'] = master_port
-    torch.distributed.init_process_group(backend='nccl', init_method='env://',
-                                         rank=rank, world_size=size)
-
-def cleanup_distributed_environment():
-    """ Cleanup the distributed environment. """
-    torch.distributed.barrier()
-    torch.distributed.destroy_process_group()
 
 def train_process(rank, size, args):
     """ The training process to be executed on each distributed node/GPU. """
